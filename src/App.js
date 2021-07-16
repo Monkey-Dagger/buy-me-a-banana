4848a2a4e0e55d0c75b3e3bee5a0200db5b77d5d
shreyas <spapinwar@gmail.com>
2021-07-16 12:04:50 +0530

    initial commit with header

---
+++ src/App.js
@@ -0,0 +1,180 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+
+import { StaticJsonRpcProvider, Web3Provider } from '@ethersproject/providers';
+import { Alert } from 'antd';
+import Web3Modal from 'web3modal';
+import './App.css';
+import WebFont from 'webfontloader';
+import { Header } from './components';
+import { INFURA_ID, NETWORK, NETWORKS } from './constants';
+import {
+  useUserProvider,
+} from './hooks';
+
+/// 📡 What chain are your contracts deployed to?
+const targetNetwork = NETWORKS.mumbai; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+
+// 😬 Sorry for all the console logging
+const DEBUG = true;
+
+// 🛰 providers
+if (DEBUG) console.log('📡 Connecting to Mainnet Ethereum');
+// const mainnetProvider = getDefaultProvider("mainnet", { infura: INFURA_ID, etherscan: ETHERSCAN_KEY, quorum: 1 });
+// const mainnetProvider = new InfuraProvider("mainnet",INFURA_ID);
+//
+// attempt to connect to our own scaffold eth rpc and if that fails fall back to infura...
+// Using StaticJsonRpcProvider as the chainId won't change see https://github.com/ethers-io/ethers.js/issues/901
+const scaffoldEthProvider = new StaticJsonRpcProvider('https://rpc.scaffoldeth.io:48544');
+const mainnetInfura = new StaticJsonRpcProvider('https://mainnet.infura.io/v3/' + INFURA_ID);
+// ( ⚠️ Getting "failed to meet quorum" errors? Check your INFURA_I
+
+// 🏠 Your local provider is usually pointed at your local blockchain
+const localProviderUrl = targetNetwork.rpcUrl;
+// as you deploy to other networks you can set REACT_APP_PROVIDER=https://dai.poa.network in packages/react-app/.env
+const localProviderUrlFromEnv = localProviderUrl;
+if (DEBUG) console.log('🏠 Connecting to provider:', localProviderUrlFromEnv);
+const localProvider = new StaticJsonRpcProvider(localProviderUrlFromEnv);
+
+/*
+  Web3 modal helps us "connect" external wallets:
+*/
+const web3Modal = new Web3Modal({
+  // network: 'mumbai', // Optional. If using WalletConnect on xDai, change network to "xdai" and add RPC info below for xDai chain.
+  cacheProvider: true, // optional
+  theme: 'dark', // optional. Change to "dark" for a dark theme.
+  providerOptions: {
+    walletconnect: {
+      package: WalletConnectProvider, // required
+      options: {
+        infuraId: INFURA_ID,
+      },
+    },
+  },
+});
+
+const logoutOfWeb3Modal = async () => {
+  await web3Modal.clearCachedProvider();
+  setTimeout(() => {
+    window.location.reload();
+  }, 1);
+};
+
+function App() {
+  const mainnetProvider = scaffoldEthProvider && scaffoldEthProvider._network ? scaffoldEthProvider : mainnetInfura;
+
+  const [injectedProvider, setInjectedProvider] = useState();
+
+  const userProvider = useUserProvider(injectedProvider, localProvider);
+
+  // You can warn the user if you would like them to be on a specific network
+  const localChainId = localProvider && localProvider._network && localProvider._network.chainId;
+  const selectedChainId = userProvider && userProvider._network && userProvider._network.chainId;
+
+  /*
+  const addressFromENS = useResolveName(mainnetProvider, "austingriffith.eth");
+  console.log("🏷 Resolved austingriffith.eth as:",addressFromENS)
+  */
+
+  //
+  // 🧫 DEBUG 👨🏻‍🔬
+  //
+  useEffect(() => {
+    if (
+      DEBUG &&
+      mainnetProvider &&
+      selectedChainId
+    ) {
+      console.log('_____________________________________ 🏗 scaffold-eth _____________________________________');
+      console.log('🌎 mainnetProvider', mainnetProvider);
+      console.log('🏠 localChainId', localChainId);
+      console.log('🕵🏻‍♂️ selectedChainId:', selectedChainId);
+    }
+  }, [
+    mainnetProvider,
+    selectedChainId,
+  ]);
+
+  let networkDisplay = '';
+  if (selectedChainId === 80001) {
+    networkDisplay = (
+      <div style={{
+        zIndex: 2, position: 'absolute', right: 10, top: 100, padding: 16, maxWidth: '400px',
+      }}
+      >
+        <Alert
+          message="⚠️ Wrong Network"
+          description={(
+            <div>
+              You have
+              {' '}
+              {' '}
+              selected wrong network and you need to be on
+              {' '}
+              <b>Polygon Mumbai testnet 80001</b>
+              .
+            </div>
+          )}
+          type="error"
+          closable={false}
+        />
+      </div>
+    );
+  } else {
+    console.log('i am in ', selectedChainId);
+    networkDisplay = (
+      <div style={{
+        zIndex: 2, position: 'absolute', right: 154, top: 28, padding: 16, color: targetNetwork.color,
+      }}
+      >
+        {targetNetwork.name}
+      </div>
+    );
+  }
+
+  const loadWeb3Modal = useCallback(async () => {
+    const provider = await web3Modal.connect();
+    setInjectedProvider(new Web3Provider(provider));
+  }, [setInjectedProvider]);
+
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      loadWeb3Modal();
+    }
+  }, [loadWeb3Modal]);
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Chewy'],
+      },
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {networkDisplay}
+      <Header web3Modal={web3Modal} loadWeb3Modal={loadWeb3Modal} logoutOfWeb3Modal={logoutOfWeb3Modal} />
+    </div>
+  );
+}
+
+/* eslint-disable */
+window.ethereum &&
+  window.ethereum.on("chainChanged", chainId => {
+    web3Modal.cachedProvider &&
+      setTimeout(() => {
+        window.location.reload();
+      }, 1);
+  });
+
+window.ethereum &&
+  window.ethereum.on("accountsChanged", accounts => {
+    web3Modal.cachedProvider &&
+      setTimeout(() => {
+        window.location.reload();
+      }, 1);
+  });
+/* eslint-enable */
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Chip from '@material-ui/core/Chip';
+import Lottie from 'react-lottie';
+import Typography from '@material-ui/core/Typography';
+
+import logo from '../assets/monkey.json';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    fontFamily: 'Chewy',
+    color: 'rgba(249, 211, 180, 1)',
+    marginLeft: '5px',
+  },
+  appBar: {
+    height: '80px',
+    borderBottom: '0.4px solid #353945',
+    background: '#141416',
+  },
+  chip: {
+    borderColor: 'rgba(249, 211, 180, 1)',
+    borderWidth: '2px',
+    fontSize: 15,
+    padding: 5,
+    height: 40,
+    borderRadius: 20,
+    color: 'rgba(249, 211, 180, 1)',
+  },
+  empty: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Header({
+  web3Modal,
+  loadWeb3Modal,
+  logoutOfWeb3Modal,
+}) {
+  const classes = useStyles();
+
+  const modalButtons = [];
+
+  if (web3Modal) {
+    if (web3Modal.cachedProvider) {
+      modalButtons.push(
+        <Chip
+          className={classes.chip}
+          label="Logout"
+          onClick={logoutOfWeb3Modal}
+          variant="outlined"
+        />,
+      );
+    } else {
+      modalButtons.push(
+        <Chip
+          className={classes.chip}
+          label="Connect Wallet"
+          onClick={loadWeb3Modal}
+          variant="outlined"
+        />,
+      );
+    }
+  } else {
+    console.log('no modal error');
+    modalButtons.push(
+      <Chip
+        className={classes.chip}
+        label="err..."
+        onClick={loadWeb3Modal}
+        variant="outlined"
+      />,
+    );
+  }
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: logo,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.appBar}>
+          <Lottie
+            options={defaultOptions}
+            height={60}
+            width={60}
+          />
+          <Typography variant="h6" className={classes.title}>
+            Buy me a banana
+          </Typography>
+          <div className={classes.empty} />
+          {modalButtons}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
